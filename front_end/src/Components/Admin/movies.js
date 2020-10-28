00723b131bba571a3954ff30ccef7aaf3b7f6841
arijitiiest <1999arijitchoudhury@gmail.com>
2020-10-28 20:50:49 +0530

    admin view changes

--- front_end/src/Components/Admin/movies.js
+++ front_end/src/Components/Admin/movies.js
@@ -1,4 +1,4 @@
-import React, { cloneElement } from "react";
+import React from "react";
 import {
   Create,
   Datagrid,
@@ -9,18 +9,35 @@ import {
   TextField,
   TextInput,
   NumberInput,
-  ArrayField,
   SingleFieldList,
+  ArrayInput,
+  SimpleFormIterator,
+  ChipField,
+  UrlField,
+  RadioButtonGroupInput,
 } from "react-admin";
 
 import Icon from "@material-ui/icons/Theaters";
+import Chip from "@material-ui/core/Chip";
+
 export const MovieIcon = Icon;
 
-export const StringToLabelObject = ({ record, children, ...rest }) =>
-  cloneElement(children, {
-    record: { label: record },
-    ...rest,
-  });
+const TextArrayField = ({ record, source }) => {
+  if (!record) return <></>;
+  const array = record[source];
+  if (typeof array === "undefined" || array === null || array.length === 0) {
+    return <div />;
+  } else {
+    return (
+      <>
+        {array.map((item) => (
+          <Chip label={item} key={item} />
+        ))}
+      </>
+    );
+  }
+};
+TextArrayField.defaultProps = { addLabel: true };
 
 export const MovieList = (props) => {
   return (
@@ -28,17 +45,31 @@ export const MovieList = (props) => {
       <Datagrid>
         <TextField source="id" />
         <TextField source="title" />
-        <TextField source="desc" />
+        {/* <TextField source="desc" /> */}
+        <TextArrayField source="genres">
+          <SingleFieldList>
+            <ChipField source="id" />
+          </SingleFieldList>
+        </TextArrayField>
+        <TextArrayField source="casts">
+          <SingleFieldList>
+            <ChipField source="id" />
+          </SingleFieldList>
+        </TextArrayField>
+        <TextArrayField source="crews">
+          <SingleFieldList>
+            <ChipField source="id" />
+          </SingleFieldList>
+        </TextArrayField>
         <TextField source="running_time" />
         <TextField source="format" />
-        <TextField source="age_level" />
-        <ArrayField source="genres">
-          <SingleFieldList >
-            <StringToLabelObject>
-              <TextField source="label" />
-            </StringToLabelObject>
+        <TextArrayField source="languages">
+          <SingleFieldList>
+            <ChipField source="id" />
           </SingleFieldList>
-        </ArrayField>
+        </TextArrayField>
+        <TextField source="age_level" />
+        {/* <UrlField source="image_url" /> */}
         <EditButton basePath="/movies" />
         <DeleteButton basePath="/movies" />
       </Datagrid>
@@ -51,9 +82,35 @@ export const MovieCreate = (props) => {
     <Create title="Create a Movie" {...props}>
       <SimpleForm>
         <TextInput source="title" />
-        <TextInput source="desc" multiLine />
+        <TextInput multiline source="desc" />
+        <ArrayInput source="genres">
+          <SimpleFormIterator>
+            <TextInput />
+          </SimpleFormIterator>
+        </ArrayInput>
+        <ArrayInput source="casts">
+          <SimpleFormIterator>
+            <TextInput />
+          </SimpleFormIterator>
+        </ArrayInput>
+        <ArrayInput source="crews">
+          <SimpleFormIterator>
+            <TextInput />
+          </SimpleFormIterator>
+        </ArrayInput>
         <NumberInput source="running_time" />
-        <TextInput source="format" />
+        <RadioButtonGroupInput
+          source="format"
+          choices={[
+            { id: "2D", name: "2D" },
+            { id: "3D", name: "3D" },
+          ]}
+        />
+        <ArrayInput source="languages">
+          <SimpleFormIterator>
+            <TextInput />
+          </SimpleFormIterator>
+        </ArrayInput>
         <NumberInput source="age_level" />
         <TextInput source="image_url" />
       </SimpleForm>
