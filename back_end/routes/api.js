a6c2e7225b55cb125bbc8bdb6b1d3e691e8e4a3b
arijitiiest <1999arijitchoudhury@gmail.com>
2020-10-29 19:52:38 +0530

    review data added

--- back_end/routes/api.js
+++ back_end/routes/api.js
@@ -8,6 +8,8 @@ const isAdminMiddleware = require("../middleware/is-Admin");
 
 const router = express.Router();
 
+router.get("/movie/:id", movieController.getMovie);
+
 router.get("/movies", rangeMiddleware, movieController.getMovies);
 
 router.post("/movies", isAdminMiddleware, movieController.postMovie);
@@ -19,6 +21,8 @@ router.get("/shows", rangeMiddleware, movieController.getShows);
 router.post("/shows", isAdminMiddleware, movieController.postShow);
 
 router.delete("/shows/:id", isAdminMiddleware, movieController.deleteShow);
+
+router.get("/review-data", movieController.getReviewData);
 
 router.get("/reviews", movieController.getReviews);
 
