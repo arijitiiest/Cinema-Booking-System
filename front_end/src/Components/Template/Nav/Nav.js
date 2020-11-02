84a181e6dc8d346f7afe80b8f70c28d6b5a159e4
markysans <dolanmaity96@gmail.com>
2020-11-02 13:57:28 +0530

    navbar styling undo

--- front_end/src/Components/Template/Nav/Nav.js
+++ front_end/src/Components/Template/Nav/Nav.js
@@ -6,28 +6,27 @@ import { LinkContainer } from 'react-router-bootstrap';
 const Nav = () => {
     return (
         <div>
-            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect style={{padding: ".5rem"}}>
-            
-              <Container fluid style={{padding: "0 3rem"}}>
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect style={{padding: '1.0rem'}}  >
               <LinkContainer to='/'>
-                <Navbar.Brand> <div style={{fontSize: 28, letterSpacing: 2}}>Cholochitro Bhobon</div></Navbar.Brand>
-              </LinkContainer>            
-              
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav" style={{fontSize: 14}}>
-                <Navb className="ml-auto">
-                <LinkContainer to='/shows'>
-                  <Navb.Link> <i className="fas fa-film"></i> Shows</Navb.Link>
-                </LinkContainer>
-                <LinkContainer to='/cart'>
-                  <Navb.Link> <i className="fas fa-shopping-cart"></i> Cart</Navb.Link>
-                </LinkContainer>
-                <LinkContainer to='/login'>
-                  <Navb.Link> <i className="fas fa-user-circle"></i> Sign In</Navb.Link>
-                </LinkContainer>  
-                </Navb>
-              </Navbar.Collapse>
-            </Container>
+                <Navbar.Brand> <div>Cholochitro Bhobon</div></Navbar.Brand>
+              </LinkContainer> 
+
+              <Container>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                  <Navb className="ml-auto">
+                    <LinkContainer to='/shows'>
+                      <Navb.Link> <i className="fas fa-film"></i> Shows</Navb.Link>
+                    </LinkContainer>
+                    <LinkContainer to='/cart'>
+                      <Navb.Link> <i className="fas fa-shopping-cart"></i> Cart</Navb.Link>
+                    </LinkContainer>
+                    <LinkContainer to='/login'>
+                      <Navb.Link> <i className="fas fa-user-circle"></i> Sign In</Navb.Link>
+                    </LinkContainer>  
+                  </Navb>
+                </Navbar.Collapse>
+              </Container>
             </Navbar>
         </div>
     )
